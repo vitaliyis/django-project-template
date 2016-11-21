289b52d2e7c39f593ef848eb23778dd35e125130
vitaliyis <vetal.friend@gmail.com>
2016-11-21 20:07:13 +0300

    part 6 dynamic students list

--- students/views.py
+++ students/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Views for Students
+def students_list(request):
+    students = (
+        {'id': 1,
+         'first_name': u'Виталий',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/me.jpeg'},
+        {'id': 2,
+         'first_name': u'Корост',
+         'last_name': u'Андрей',
+         'ticket': 2123,
+         'image': 'img/piv.png'},
+        {'id': 3,
+         'first_name': u'Тарас',
+         'last_name': u'Притула',
+         'ticket': 5332,
+         'image': 'img/podoba3.jpg'},
+    )
+    return render(request, 'students/students_list.html', {'students': students})
+
+def students_add(request):
+    return HttpResponse('<h1>Students Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Students %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Students %s</h1>' % sid)
+
+# Views for Groups
+def groups_list(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+
+
+
+
+
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf.urls import include, url
 from django.contrib import admin
+from students import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Students urls
+    url(r'^$', views.students_list, name='home'),
+    url(r'^students/add/$', views.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name='students_delete'),
+
+    # Groups urls
+    url(r'groups/$', views.groups_list, name='groups'),
+    url(r'groups/add/$', views.groups_add, name='groups_add'),
+    url(r'groups/(?P<gid>\d+)/edit/$', views.groups_edit, name='groups_edit'),
+    url(r'groups/(?P<gid>\d+)/delete/$', views.groups_delete, name='groups_delete'),
+
+    url(r'^admin/', include(admin.site.urls)),
 ]
+
+#urlpatterns = patterns('',
+#    # Students urls
+#    url(r'^$', 'students.views.students_list', name='home'),
+
+#    url(r'^admin/', include(admin.site.urls)),
+#)
